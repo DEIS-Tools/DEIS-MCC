b929b54769a642e199448c0ac8842ae8ec431cb4
Morten Konggaard Schou <mksc@cs.aau.dk>
2026-03-10 10:23:51 +0100

    Update simple.sh

--- example-scripts/simple.sh
+++ example-scripts/simple.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=hello_world  # Give your experiment a name
 #SBATCH --mail-type=FAIL  # Type of email notification: BEGIN,END,FAIL,ALL,NONE
 #SBATCH --mail-user=<YOUR-EMAIL>
-#SBATCH --partition=naples,dhabi,rome  # Which partitions may your job be scheduled on
+#SBATCH --partition=naples,rome,genoa  # Which partitions may your job be scheduled on
 #SBATCH --time=1:00:00  # (Optional) time limit in dd:hh:mm:ss format. Make sure to keep an eye on your jobs (using 'squeue -u $(whoami)') anyways.
 #SBATCH --mem=1G  # Memory limit that slurm allocates
 
 # Memory limit for user program. Must be equal to SBATCH-directive allocation.
 #  (allows graceful handling of out-of-memory errors in your program.)
-let "m=1024*1024*1"
+let "m=1024*${SLURM_MEM_PER_NODE}"
 ulimit -v $m
 
 # Print info to stdout on job details
@@ -16,4 +16,5 @@ echo "Job-name: $SLURM_JOB_NAME; jobid: $SLURM_JOB_ID; Partition: $SLURM_JOB_PAR
 
 #######################
 # Your code goes below #
+
 #######################
