a6593c12bd03324912ae1ee1d7670364e94da293
Morten Konggaard Schou <mksc@cs.aau.dk>
2026-03-10 10:36:20 +0100

    Update python-venv-example.sh

--- example-scripts/python-venv-example.sh
+++ example-scripts/python-venv-example.sh
@@ -3,20 +3,26 @@
 #SBATCH --mail-user=<YOUR-EMAIL>
 #SBATCH --output=/nfs/home/cs.aau.dk/<YOUR-ACCOUNT-NAME>/slurm-output/python-venv-example-%j.out  # Redirect the output stream to this file (%j is the jobid)
 #SBATCH --error=/nfs/home/cs.aau.dk/<YOUR-ACCOUNT-NAME>/slurm-output/python-venv-example-%j.err   # Redirect the error stream to this file (%j is the jobid)
-#SBATCH --partition=naples,dhabi,rome  # Which partitions may your job be scheduled on
+#SBATCH --partition=naples,rome,genoa  # Which partitions may your job be scheduled on
 #SBATCH --mem=1G  # Memory limit that slurm allocates
-#SBATCH --time=1:00:00  # (Optional) time limit in dd:hh:mm:ss format. Make sure to keep an eye on your jobs (using 'squeue -u $(whoami)') anyways.
+#SBATCH --time=1:00:00  # (Optional) time limit in dd:hh:mm:ss format. Make sure to keep an eye on your jobs (using 'squeue -u $(whoami)') anyway.
 
 # Memory limit for user program
-let "m=1024*1024*1"  
+let "m=1024*${SLURM_MEM_PER_NODE}"
 ulimit -v $m
 
 U=$(whoami)
 PD=$(pwd)
-# Create a unique folder for this job execution in your scratch folder.
-SCRATCH_DIRECTORY=/scratch/${U}/${SLURM_JOBID}  
-mkdir -p ${SCRATCH_DIRECTORY}
-cd ${SCRATCH_DIRECTORY}
+# Create a unique folder for this job execution in your scratch folder. (In case your program writes temporary files.)
+SCRATCH_DIR="/scratch/${U}"
+JOB_DIR="${SCRATCH_DIR}/${SLURM_JOB_ID}"
+# Clean up after yourself, in case any temporary files were written, even if the job is killed prematurely.
+cleanup () {
+    rm -rf ${JOB_DIR} 2>/dev/null >/dev/null
+}
+trap cleanup EXIT KILL
+mkdir -p ${JOB_DIR}
+cd ${JOB_DIR}
 
 # Activate python virtual environment
 python3 -m venv venv
@@ -35,8 +41,3 @@ python -m pip install -r requirements.txt
 ##########################
 
 # Maybe you need to copy a result file back to ${PD}
-
-
-# Clean up after yourself
-cd /scratch/${U}
-[ -d "${SLURM_JOBID}" ] && rm -r ${SLURM_JOBID}
